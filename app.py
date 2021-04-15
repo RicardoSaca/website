b2b02c03f20ce75c7c083921348d383b0988c661
Ricardo Saca <ricardosaca@gmail.com>
2021-04-15 14:58:49 -0400

    added links to sections of website

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contactme')
+def contact():
+    return render_template("contact.html")
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template("portfolio.html")
