1c5115a6f98c5b9f2c200268c8eb58c1d09e1d5c
Ricardo <ricardosaca@gmail.com>
2021-05-24 22:22:55 -0400

    Add contact form and minor changes

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash
 from flask_fontawesome import FontAwesome
+from forms import ContactForm
+import pandas as pd
 
 app = Flask(__name__)
+app.secret_key = 'secretKey'
 fa = FontAwesome(app)
 
 @app.route('/')
@@ -12,9 +15,29 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/contactme')
+# @app.route('/contactme', methods=["GET","POST"])
+# def contact():
+#     form = ContactForm()
+#     if request.method == 'POST':
+#         name = request.form["name"]
+#         email = request.form["email"]
+#         subject = request.form["subject"]
+#         message = request.form["message"]
+#         res = pd.DataFrame({'name':name, 'email':email, 'subject':subject, 'message':message}, index=[0])
+#         res.to_csv('./contactusMessage.csv')
+#         print("The data are saved!")
+#         return render_template('contact.html', form=form)
+#     else:
+#         return render_template("contact.html", form=form)
+
+@app.route('/contactme', methods=['GET','POST'])
 def contact():
-    return render_template("contact.html")
+    form = ContactForm()
+
+    if request.method == 'POST':
+        return 'Form posted.'
+    elif request.method == 'GET':
+        return  render_template('contact.html', form=form)
 
 @app.route('/portfolio')
 def portfolio():
